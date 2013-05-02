e1894beab06663f035cad9cc48ee7fde770bfb42
Kyle DeTella <kyle@wcst.com>
2013-05-02 09:00:50 -0500

    undefined

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,37 @@ module.exports = function(grunt) {
 
 
     clean: {
-      dist: 'dist'
+      // dist: 'dist'
+      clean: ["./public/dist"]
+    },
+
+    //
+    // Build & Compile LESS
+    //
+    less: {
+      development: {
+        options: {
+          paths: ["./public/css/less"],
+          yuicompress: false
+        },
+        files: {
+          "./public/dist/css/app.css": "./public/css/less/app.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ["./public/css/less"],
+          yuicompress: true
+        },
+        files: {
+          "./public/dist/css/app.css": "./public/css/less/app.less"
+        }
+      }
+    },
+
+    watch: {
+      files: "./public/css/less/*",
+      tasks: ["less"]
     }
 
   });
@@ -69,11 +99,14 @@ module.exports = function(grunt) {
   // Enter as grunt release:minor (or major, patch, build)
   // This will re-write and update package.json
   //
-  grunt.registerTask('release', 'Ship it.', function(version) {
+  grunt.registerTask('release', 'Ship (dev & production)', function(version) {
 
     var _ = grunt.util._,
         pkg = grunt.file.readJSON('package.json'),
         curVersion = pkg.version;
+
+    if(!version) version = 'minor';
+
     version = semver.inc(curVersion, version) || version;
 
     if (!semver.valid(version) || semver.lte(version, curVersion)) {
@@ -90,6 +123,7 @@ module.exports = function(grunt) {
     grunt.task.run([
       'clean',
       'requirejs',
+      'less:production',
       'exec:git_add',
       'exec:git_commit',
       'exec:git_push'
@@ -103,14 +137,17 @@ module.exports = function(grunt) {
   grunt.registerTask('sweep', 'Clean up files', function() {
     grunt.task.run([
       'clean',
-      'requirejs',
       'exec:git_add_u'
     ]);
   });
 
 
+  //
+  // Register Grunt Tasks
+  //
   grunt.registerTask('default', 'build');
-  grunt.registerTask('build', ['requirejs','sed:version']);
+  grunt.registerTask('build', ['requirejs', 'less:development', 'sed:version']);
+  grunt.registerTask('watch', ['less:development']);
 
   //
   // Load npm tasks
@@ -120,6 +157,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
 
 
